c2f59829a760f3f76e1243f85909628112605670
Samit Koyom <samitkoyom@gmail.com>
2024-09-02 21:40:36 +0700

    update config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,8 @@
-export const BASE_URL = 'http://192.168.88.146:3000/api/'
-export const BASE_URL_IMAGE = 'http://192.168.88.146:3000/uploads/images/'
+// export const BASE_URL = 'http://192.168.88.146:3000/api/'
+// export const BASE_URL_IMAGE = 'http://192.168.88.146:3000/uploads/images/'
+
 // export const BASE_URL = 'https://nodejs-server-api.onrender.com/api/'
+// export const BASE_URL_IMAGE = 'https://nodejs-server-api.onrender.com/uploads/images/'
+
+export const BASE_URL = 'https://samit-node-serverapi-f9jj9glav-iamsamitdev.vercel.app/api/'
+export const BASE_URL_IMAGE = 'https://samit-node-serverapi-f9jj9glav-iamsamitdev.vercel.app/uploads/images/'
